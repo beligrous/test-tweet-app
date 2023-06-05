4381411f7b131b2e28be71faecc7180447014d44
Ihor <108004180+beligrous@users.noreply.github.com>
2023-06-05 22:35:29 +0300

    +

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-tweet-app/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
