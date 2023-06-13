c1ee9e3bf3cd63ce0d374ced0adbfac16812236a
Ihor <108004180+beligrous@users.noreply.github.com>
2023-06-13 15:00:52 +0300

    fix router

--- src/components/Cart/cart.styled.js
+++ src/components/Cart/cart.styled.js
@@ -51,6 +51,7 @@ export const Button = styled.button`
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
   border-radius: 10.3108px;
   padding: 14px 0;
+  font-family: inherit;
   font-weight: 600;
   font-size: 18px;
   line-height: 22px;
